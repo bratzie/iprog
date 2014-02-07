22cb023e77baad846a0ccddfd31c1339c5626e1d
bratzie <xumbik@gmail.com>
2014-02-07 21:06:21 +0100

    Structure fixed

--- lab2/dinnerplanner-swing/src/se/kth/csc/iprog/dinnerplanner/swing/DinnerPlanner.java
+++ lab2/dinnerplanner-swing/src/se/kth/csc/iprog/dinnerplanner/swing/DinnerPlanner.java
@@ -9,59 +9,37 @@ import java.awt.*;
 
 public class DinnerPlanner extends JFrame {
 
-    // testing stuff
-    static Object[][] tempData = {
-            {"Potato", "5st", "102kr"},
-            {"Sauce", "2st", "1122kr"},
-            {"Pineapple", "1st", "12302kr"},
-            {"Axel", "10st", "80085kr"},
-            {"Potato", "5102st", "2kr"}
-    };
-
 	private static final long serialVersionUID = 1L;
 
 	public DinnerPlanner() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}	
-	
-	private static DinnerModel model = new DinnerModel();
+	}
 
+
+    // fix model issues
+	private static DinnerModel model = new DinnerModel();
 	public DinnerModel getModel() {
 		return model;
 	}
-
 	public void setModel(DinnerModel model) {
 		this.model = model;
 	}
 
-    /**
-     * Main method, adding comment to test git. Now I'm testing again.
-     *
-     * @param args
-     */
+    // initialize panels
+    static JTabbedPane courseSwitcher = new JTabbedPane();
+    static MainView mainView = new MainView();
+    static IngredientView ingredientView = new IngredientView(model.getSelectedDish(2));
+    static OptionsView optionsView = new OptionsView();
+
 	public static void main(String[] args) {
 		//Initiating the main JFrame
 		DinnerPlanner dinnerPlanner = new DinnerPlanner();
 		dinnerPlanner.setTitle("Dinner Planner");
-		
-		//Creating the first view
-		MainView mainView = new MainView();
-        IngredientView ingredientView = new IngredientView(model.getSelectedDish(2));
-        OptionsView optionsView = new OptionsView();
 
-        // different course views
-        CourseView starterCourseView = new CourseView();
-        CourseView mainCourseView = new CourseView();
-        CourseView dessertCourseView = new CourseView();
-
-        // add the course views to different tabs
-        JTabbedPane courseSwitcher = new JTabbedPane();
-        courseSwitcher.add(starterCourseView, "Starter");
-        courseSwitcher.add(mainCourseView, "Main");
-        courseSwitcher.add(dessertCourseView, "Dessert");
+        initTabbedPane();
 		
 		//Adding the view to the main JFrame
-		dinnerPlanner.getContentPane().add(optionsView);
+		dinnerPlanner.getContentPane().add(courseSwitcher);
 
         // make sure it's not resizable
         dinnerPlanner.setResizable(true);
@@ -72,6 +50,18 @@ public class DinnerPlanner extends JFrame {
 		
 		//and starting the JFrame
 		dinnerPlanner.setVisible(true);
-	}
+    }
+
+    private static void initTabbedPane() {
+        // different course views
+        CourseView starterCourseView = new CourseView();
+        CourseView mainCourseView = new CourseView();
+        CourseView dessertCourseView = new CourseView();
+
+        // add the course views to different tabs
+        courseSwitcher.add(starterCourseView, "Starter");
+        courseSwitcher.add(mainCourseView, "Main");
+        courseSwitcher.add(dessertCourseView, "Dessert");
+    }
 
 }
