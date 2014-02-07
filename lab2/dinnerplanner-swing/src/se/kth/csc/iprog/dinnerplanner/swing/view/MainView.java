9b863fb7558d77a938e925c34ed268bff08c9f30
bratzie <xumbik@gmail.com>
2014-02-07 16:13:35 +0100

    Layout is almost okay now

--- lab2/dinnerplanner-swing/src/se/kth/csc/iprog/dinnerplanner/swing/view/MainView.java
+++ lab2/dinnerplanner-swing/src/se/kth/csc/iprog/dinnerplanner/swing/view/MainView.java
@@ -22,56 +22,60 @@ public class MainView extends JPanel {
 	
 	public MainView(){
 
+        // main container panel
         topPanel = new JPanel(new BorderLayout());
         topPanel.setPreferredSize(new Dimension(1024, 768));
 
+        // food panels
         starterPanel = new JPanel(new BorderLayout());
         mainPanel = new JPanel(new BorderLayout());
         dessertPanel = new JPanel(new BorderLayout());
 
-        optionsPanel = new JPanel(new GridLayout(3, 1));
-        guestCostPanel = new JPanel(new GridLayout(2,2));
-        menuPanel = new JPanel(new BorderLayout());
+        // options panel (to the right)
+        optionsPanel = new JPanel(new GridLayout(3, 1)); // container
+        guestCostPanel = new JPanel(new GridLayout(2,2)); // top panel with guests and cost
+        menuPanel = new JPanel(new BorderLayout()); // showing full menu
 
-        optionsPanel.setMinimumSize(new Dimension(200, 768));
-        optionsPanel.setMaximumSize(new Dimension(200, 768));
-
-        topPanel.add(tabbedPanel, BorderLayout.CENTER);
-        topPanel.add(optionsPanel, BorderLayout.EAST);
-
-        tabbedPanel.addTab("Starter", starterPanel);
-        tabbedPanel.addTab("Main", mainPanel);
-        tabbedPanel.addTab("Dessert", dessertPanel);
+        // init guest/cost panel
+        JComboBox numOfGuests = new JComboBox();
+        JLabel numGuestsTitle = new JLabel("Number of guests:");
+        JLabel totCostTitle = new JLabel("Total cost:");
+        JTextField costField = new JTextField("100€");
+        guestCostPanel.add(numGuestsTitle);
+        guestCostPanel.add(numOfGuests);
+        guestCostPanel.add(totCostTitle);
+        guestCostPanel.add(costField);
 
         // init menu panel
         JLabel menuTitle = new JLabel("Dinner Menu");
         JScrollPane menuScroll = new JScrollPane();
         menuPanel.add(menuTitle, BorderLayout.NORTH);
         menuPanel.add(menuScroll, BorderLayout.CENTER);
-        menuPanel.setPreferredSize(new Dimension(200, 500));
+        menuPanel.setMinimumSize(new Dimension(200, 500));
 
-        // init guest/cost panel
-        JComboBox numOfGuests = new JComboBox();
-        JLabel numGuestsTitle = new JLabel("Number of guests:");
-        JLabel totCostTitle = new JLabel("Total cost:");
-        guestCostPanel.add(numGuestsTitle);
-        guestCostPanel.add(numOfGuests);
-        guestCostPanel.add(totCostTitle);
-
-
-
-        // init options panel
-        optionsPanel.add(guestCostPanel);
-
+        // init button panel
         JPanel buttonPanel = new JPanel(new GridLayout(1, 2));
+        buttonPanel.setMaximumSize(new Dimension(200, 50));
         JButton ingButton = new JButton("Ingredients");
         JButton prepButton = new JButton("Preparation");
         buttonPanel.add(ingButton);
         buttonPanel.add(prepButton);
-        buttonPanel.setPreferredSize(new Dimension(200, 50));
 
+        // init options panel
+        optionsPanel.add(guestCostPanel);
         optionsPanel.add(menuPanel);
         optionsPanel.add(buttonPanel);
+        optionsPanel.setMinimumSize(new Dimension(200, 768));
+        optionsPanel.setMaximumSize(new Dimension(200, 768));
+
+        // add panels to the main container panel
+        topPanel.add(tabbedPanel, BorderLayout.CENTER);
+        topPanel.add(optionsPanel, BorderLayout.EAST);
+
+        // add tabs to the tabbed panel
+        tabbedPanel.addTab("Starter", starterPanel);
+        tabbedPanel.addTab("Main", mainPanel);
+        tabbedPanel.addTab("Dessert", dessertPanel);
 
         // search field
         JTextField searchField1 = new JTextField("");
@@ -79,7 +83,6 @@ public class MainView extends JPanel {
         JTextField searchField3 = new JTextField("");
 
         // init starter panel
-
         JPanel dishPanelStart = new JPanel(new GridLayout(0, 4));
         JScrollPane dishContainerStart = new JScrollPane();
         dishContainerStart.add(dishPanelStart);
@@ -88,12 +91,20 @@ public class MainView extends JPanel {
         starterPanel.add(dishContainerStart, BorderLayout.CENTER);
 
         // init main panel
+        JPanel dishPanelMain = new JPanel(new GridLayout(0, 4));
+        JScrollPane dishContainerMain = new JScrollPane();
+        dishContainerMain.add(dishPanelMain);
+
         mainPanel.add(searchField2, BorderLayout.NORTH);
-        mainPanel.add(dishContainerStart, BorderLayout.CENTER);
+        mainPanel.add(dishContainerMain, BorderLayout.CENTER);
 
         // init dessert panel
+        JPanel dishPanelDessert = new JPanel(new GridLayout(0, 4));
+        JScrollPane dishContainerDessert = new JScrollPane();
+        dishContainerDessert.add(dishPanelDessert);
+
         dessertPanel.add(searchField3, BorderLayout.NORTH);
-        dessertPanel.add(dishContainerStart, BorderLayout.CENTER);
+        dessertPanel.add(dishContainerDessert, BorderLayout.CENTER);
 
         /*
         // dishes
