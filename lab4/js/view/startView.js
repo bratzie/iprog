624e531722a587f5f937263c8d625893e63f93d9
elvea <josefin.agerblad@gmail.com>
2014-02-25 16:05:05 +0100

    Started creating a layout

--- lab4/js/view/startView.js
+++ lab4/js/view/startView.js
@@ -1,4 +1,10 @@
 var StartView = function (container,model) {
 
 	this.createDinnerBtn = container.find("#createDinner");
+
+	//creating the layout of the view
+	var div = $("<div>");
+	div.html("Homelette");
+	container.append(div);
+
 }
