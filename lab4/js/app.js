4bf847bcee064f12f953b97358ad8a3d426e05f0
elvea <josefin.agerblad@gmail.com>
2014-02-25 14:58:44 +0100

    changes in app.js and in index.html
    removed the code that didnt belong in index and instantiated the start
    view and controller.

--- lab4/js/app.js
+++ lab4/js/app.js
@@ -8,4 +8,6 @@ $(function() {
 	//And create the needed controllers and views
 	var exampleView = new ExampleView($("#exampleView"),model);
    	var exampleViewController = new ExampleViewController(exampleView,model);
+   	var startView = new StartView($("#startView"),model);
+   	var startViewController = new StartViewControlle(startView,model);
 });
