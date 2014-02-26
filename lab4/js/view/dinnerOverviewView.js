6c1e247ccdbbc969d63040b50976ddf9d3a875c4
Axel Samuelsson <axelsam@kth.se>
2014-02-26 14:55:57 +0100

    More work on overview

--- lab4/js/view/dinnerOverviewView.js
+++ lab4/js/view/dinnerOverviewView.js
@@ -1,22 +1,33 @@
 var DinnerOverviewView = function (container, model) {
 
-	this.numberOfGuests = container.find("#overviewNumberOfGuests");
-	this.totalPrice = container.find("#overviewTotalPrice");
-	
-	//Set the inital values of the components
-	this.numberOfGuests.html(model.getNumberOfGuests());
-	this.totalPrice.html(model.getTotalMenuPrice());
+    this.numberOfGuests = container.find("#overviewNumberOfGuests");
+    this.totalPrice = container.find("#overviewTotalPrice");
+    this.menu = container.find("#overviewMenu");
+    
+    //Set the inital values of the components
+    this.numberOfGuests.html(model.getNumberOfGuests());
+    this.totalPrice.html(model.getTotalMenuPrice());
 
-	/*****************************************  
-	      Observer implementation    
-	*****************************************/
-	
-	//Register an observer to the model
-	model.addObserver(this);
-	
-	//This function gets called when there is a change at the model
-	this.update = function(arg){
-		this.numberOfGuests.html(model.getNumberOfGuests());
-		this.totalPrice.html(model.getTotalMenuPrice());
-	}
+    /*****************************************  
+          Observer implementation    
+    *****************************************/
+    
+    //Register an observer to the model
+    model.addObserver(this);
+    
+    //This function gets called when there is a change at the model
+    this.update = function(arg){
+        this.numberOfGuests.html(model.getNumberOfGuests());
+        this.totalPrice.html(model.getTotalMenuPrice());
+
+        var menu = model.getFullMenu();
+        console.log(menu);
+
+        menu.forEach(function (entry) {
+            var el = "<div class=\"col-md-3\">";
+            el += entry.name;
+            el += "</div>";
+            this.menu.append(el);
+        });
+    }
 }
--- lab4/js/view/startView.js
+++ lab4/js/view/startView.js
@@ -2,11 +2,11 @@ var StartView = function (container,model) {
 
 	this.createDinnerBtn = container.find("#createDinner");
 
-}
-
 	//Register an observer to the model
 	model.addObserver(this);
 	
 	//This function gets called when there is a change at the model
 	this.update = function(arg){
 	}
+
+}
