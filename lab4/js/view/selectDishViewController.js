e2a7440c47d1a3af88da8261a0a1e3763d8b1bf9
elvea <josefin.agerblad@gmail.com>
2014-02-26 14:52:01 +0100

    Drop down menu now works

--- lab4/js/view/selectDishViewController.js
+++ lab4/js/view/selectDishViewController.js
@@ -2,4 +2,8 @@ var SelectDishViewController = function(view, model ) {
 	$('#confirmDinner').click(function () {
 		goToView('dinnerOverviewView');
 	});
+
+	$('#noGuests').change(function() {
+		model.setNumberOfGuests($('#noGuests').val());
+	});
 }
