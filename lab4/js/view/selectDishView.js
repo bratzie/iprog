475ad2c8efca534ad063006ad1d956e0f54d594d
elvea <josefin.agerblad@gmail.com>
2014-02-26 13:29:37 +0100

    Total price label is now beautiful

--- lab4/js/view/selectDishView.js
+++ lab4/js/view/selectDishView.js
@@ -1,20 +1,7 @@
 var SelectDishView = function (container,model) {
 
 	this.numberOfGuests = container.find("#numberOfGuests");
-//div we just store in temporary variable because we won't need it later NOT READY TODO
-	var div = $("<div>");
-	//we set the constant text
-	div.html("Total menu price ");
-	//and we add the text-primary class to make it blue
-	div.addClass("text-primary");
-	//total price we store in object variable (using this) so we can access it later
-	this.totalPrice = $("<span>");
-	//we set the id of the total price span
-	this.totalPrice.attr("id","totalPrice");
-	//we add total price span to the div
-	div.append(this.totalPrice);
-	//finally we add the div to the view container
-	container.append(div);
+	this.totalPrice = container.find("#totalPrice");
 	
 	//Set the inital values of the components
 	this.numberOfGuests.html(model.getNumberOfGuests());
