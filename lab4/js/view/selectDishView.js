b50306b44f0bf219060f9c65e34667d816820cf3
elvea <josefin.agerblad@gmail.com>
2014-02-26 11:49:54 +0100

    Started out on some retrieval from model to view

--- lab4/js/view/selectDishView.js
+++ lab4/js/view/selectDishView.js
@@ -1,3 +1,35 @@
 var SelectDishView = function (container,model) {
 
+	this.numberOfGuests = container.find("#numberOfGuests");
+//div we just store in temporary variable because we won't need it later NOT READY TODO
+	var div = $("<div>");
+	//we set the constant text
+	div.html("Total menu price ");
+	//and we add the text-primary class to make it blue
+	div.addClass("text-primary");
+	//total price we store in object variable (using this) so we can access it later
+	this.totalPrice = $("<span>");
+	//we set the id of the total price span
+	this.totalPrice.attr("id","totalPrice");
+	//we add total price span to the div
+	div.append(this.totalPrice);
+	//finally we add the div to the view container
+	container.append(div);
+	
+	//Set the inital values of the components
+	this.numberOfGuests.html(model.getNumberOfGuests());
+	this.totalPrice.html(model.getTotalMenuPrice());
+
+	/*****************************************  
+	      Observer implementation    
+	*****************************************/
+	
+	//Register an observer to the model
+	model.addObserver(this);
+	
+	//This function gets called when there is a change at the model
+	this.update = function(arg){
+		this.numberOfGuests.html(model.getNumberOfGuests());
+		this.totalPrice.html(model.getTotalMenuPrice());
+	}
 }
