9cf5e18555d85de9dcdab32b6629186a339f26e2
Axel Samuelsson <axelsam@kth.se>
2014-02-27 00:00:42 +0100

    added somewhat working menu to selectDishView

--- lab4/js/view/selectDishView.js
+++ lab4/js/view/selectDishView.js
@@ -2,10 +2,7 @@ var SelectDishView = function (container,model) {
 
 	this.numberOfGuests = container.find("#numberOfGuests");
 	this.totalPrice = container.find("#totalPrice");
-	
-	//Set the inital values of the components
-	this.numberOfGuests.html(model.getNumberOfGuests());
-	this.totalPrice.html(model.getTotalMenuPrice());
+	this.menu = container.find("#selectedMenu")
 
 	/*****************************************  
 	      Observer implementation    
@@ -18,5 +15,35 @@ var SelectDishView = function (container,model) {
 	this.update = function(arg){
 		this.numberOfGuests.html(model.getNumberOfGuests());
 		this.totalPrice.html(model.getTotalMenuPrice());
+
+		this.menu.html("");
+
+        var menuarray = model.getFullMenu();
+        var el = "";
+        if(menuarray.length === 0) {
+        	el += "<div class=\"menuItem row\">";
+            el += "<div class=\"col-md-6\">Pending</div>";
+            el += "<div class=\"col-md-4 col-md-offset-2\" style=\"text-align: right;\">0</div>";
+            el += "</div>\n";
+        }
+        else {
+        	menuarray.forEach(function (dish) {
+        		var price = 0;
+        		dish.ingredients.forEach(function (ingredient) {
+        			price += ingredient.price;
+        		});
+        		price *= model.getNumberOfGuests();
+
+        		el += "<div class=\"menuItem row\">";
+        		el += "<div class=\"col-md-6\">"+dish.name+"</div>";
+        		el += "<div class=\"col-md-4 col-md-offset-2\" style=\"text-align: right;\">"+price+"</div>";
+        		el += "</div>\n";
+        	});
+        }
+
+        this.menu.html(el);
 	}
+	
+	//Set the inital values of the components
+	this.update();
 }
